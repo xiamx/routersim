94e704aa2bf99d66572baecf53056511411fb56a
Mengxuan Xia <xiamx2004@gmail.com>
2014-11-26 14:18:56 -0800

    checkpoint

--- index.js
+++ index.js
@@ -17,17 +17,40 @@ var getRouterKey = function(processIP, processPort) {
 
 attachedClients = {};
 routers = {};
+links = [];
 
-var genSendHello = function(dstIP) {
-    var helloPacket = {
-        type: 'hello',
+var getRouterSelf = function(){
+    return {
+        processIP: server.address().address,
+        processPort: server.address().port,
+        emulatedip: nconf.get('emulatedip')
+    };
+};
+
+var genPacketCommon = function(params) {
+    return {
         srcProcessIP: server.address().address,
         srcProcessPort: server.address().port,
         srcIP: nconf.get('emulatedip'),
-        dstIP: dstIP
+        dstIP: params.dstIP
     };
+};
+
+var genSendHello = function(dstIP) {
+    var helloPacket = _.extend(genPacketCommon({dstIP: dstIP}), {
+        type: 'hello'
+    });
     return function(socket) {
         socket.write(JSON.stringify(helloPacket));
+    };
+};
+
+var genSendDD = function(dstIP, withAbstract) {
+    var packet = _.extend(genPacketCommon({dstIP: dstIP}), {
+        type: 'DD'
+    });
+    return function(socket) {
+        socket.write(JSON.stringify(packet));
     };
 };
 
@@ -41,19 +64,39 @@ var processHello = function(data, connection) {
             emulatedip: data.srcIP
         };
         genSendHello(data.srcIP)(connection);
-    } else {
-        if (routers[key].status === 'INIT') {
-            routers[key] = {
-                status: 'TWO_WAY'
-            };
-            genSendHello(data.srcIP)(connection);
-        }
+    } else if (routers[key].status === 'INIT') {
+        routers[key] = _.extend(routers[key], {
+            status: 'TWO_WAY'
+        });
+        genSendHello(data.srcIP)(connection);
+    } else if (routers[key].status === 'TWO_WAY') {
+        routers[key] = _.extend(routers[key], {
+            status: 'EXSTART'
+        });
+        genSendDD(data.srcIP, false)(connection);
+    }
+};
+
+var processDD = function(data, connection) {
+    key = getRouterKey(data.srcProcessIP, data.srcProcessPort);
+    if (routers[key].status === 'TWO_WAY') {
+        routers[key] = _.extend(routers[key], {
+            status: 'EXSTART'
+        });
+        genSendDD(data.srcIP, false)(connection);
+    } else if (routers[key].status === 'EXSTART') {
+        routers[key] = _.extend(routers[key], {
+            status: 'EXCHANGE'
+        });
+        genSendDD(data.srcIP, true)(connection);
     }
 };
 
 var processData = function(data, connection) {
     if (data.type === 'hello') {
         processHello(data, connection);
+    } else if (data.type === 'DD') {
+        processDD(data, connection);
     }
 };
 
@@ -86,18 +129,19 @@ attach = function(remoteip, remoteport, emulatedip, weight) {
         processData(data, client);
     });
     client.on('end', function() {
-        winston.info('client disconnected', data);
+        winston.info('client disconnected');
     });
-    attachedClients[getRouterKey(remoteip, remoteport)] = {
+    routers[getRouterKey(remoteip, remoteport)] = {
         processIP: remoteip,
         processPort: remoteport,
         emulatedip: emulatedip,
+        status: 'INIT',
         socket: client
     };
 };
 
 start = function() {
-    _.each(attachedClients, function(client){
+    _.each(routers, function(client){
         genSendHello(client.emulatedip)(client.socket);
     });
 };
